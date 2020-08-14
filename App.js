d9c12e3d0d0f038d752807b52d37ca26fddb88f5
omergal99 <omergal99gm@gmail.com>
2020-08-14 19:58:58 +0300

    init counter

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [count, setCount] = React.useState(10);
+
   return (
     <View style={styles.container}>
+
+      <View style={{ display: 'flex', flexDirection: 'column', gap: '10px', padding: '10px' }}>
+        <Button title={'Increase'} onPress={() => setCount(count + 1)} />
+        <Button title={'Decrease'} onPress={() => setCount(count - 1)} />
+        <Text>Points:{count}</Text>
+      </View>
+
+      <View style={{ width: '100%', borderTopColor: 'black', borderTopWidth: 1, padding: '6px' }} />
+
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
